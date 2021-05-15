4bf3c730a19e7a62038756e3af1884667b9c957b
rishivijayv <rishivijayv@gmail.com>
2021-05-14 20:28:43 -0400

    Created basic setup for TypeORM
    1. Created User entitiy with basic columns
    2. Created a basic configuration for TypeORM with env vars

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-console.log("Hello from Limage. This is your captain speaking.");
-console.log(`Logging message from environment: ${process.env.TEST}`);
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from './entities/User';
+import { __prod__ } from './constants';
+
+async function main(){
+
+    const con = await createConnection({
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT!),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [ User ],
+        synchronize: !__prod__,
+        logNotifications: true
+    });
+
+
+}
+
+main();
